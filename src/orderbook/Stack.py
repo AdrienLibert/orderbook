9da353ed8eb469e5c893717e6029d96d10923869
E229866E <adrien.libert@etu.univ-nantes.fr>
2024-11-22 12:12:15 +0800

    orderbook kafka/entrypoint

---
+++ src/orderbook/Stack.py
@@ -0,0 +1,43 @@
+# Stack class for bid and ask
+class Stack:
+    def __init__(self,is_bid = True):
+        self.order = []
+        self.is_bid = is_bid # True for bid, False for ask because bid is in descending order and ask is in ascending order
+
+    def is_empty(self):
+        return len(self.order) == 0
+    
+    def sort(self):
+        self.order.sort(key = lambda x: x['Price'], reverse = self.is_bid)
+
+    def push(self, price, quantity):
+        for i in range(len(self.order)):
+            if self.order[i]['Price'] == price:
+                self.order[i]['Quantity'] += quantity
+                return
+        self.order.append({'Price' : price, 'Quantity' : quantity})
+        self.sort()
+    
+    def pop(self):
+        if self.is_empty():
+            return None
+        return self.order.pop(0)
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.order[0]
+    
+    def size(self):
+        return len(self.order)
+    
+    def __str__(self):
+        return str(self.order)
+    
+    def to_dict(self):
+        return {
+            "order": self.order
+        }
+    
+    def __repr__(self):
+        return json.dumps(self.to_dict())  # Pour une conversion JSON automatique
