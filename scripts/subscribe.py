82eb581cea728c84536a54c10f0c2080b2296203
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2024-12-17 22:22:42 +0800

    improve consumer script for end of file and commit only on valid messages

--- scripts/subscribe.py
+++ scripts/subscribe.py
@@ -1,28 +1,36 @@
 # This script is intended to be modified, the goal is to have manual producer to kafka
 # Feel free to edit values / whatever for your testing purposes
-from confluent_kafka import Consumer, TopicPartition
+from confluent_kafka import Consumer, TopicPartition, KafkaError
 
 from drgn.kafka import kafka_config
 import time
+import uuid
 
 
 # defined in topics_config.yaml. if changed, need to rebuild the image.
 TOPIC = "orders.topic"
 
+def commit(err, topics):
+    print(f"error on commit: {err}: {topics}")
 
 def consume_orders(consumer: Consumer):
     while True:
-        msg = consumer.poll(1)
+        msgs = consumer.consume()
 
-        if msg:
-            if msg.error():
-                print(msg.error)
-            else:
-                print(msg.timestamp(), msg.offset(), msg.value())
+        if len(msgs) > 0:
+            for msg in msgs:
+                if msg.error():
+                    code = msg.error().code() 
+                    if code == KafkaError._PARTITION_EOF:
+                        print(f"No more msgs")
+                    else:
+                        print(code)
+                else:
+                    print(msg.timestamp(), msg.offset(), msg.value())
+                    consumer.commit(message=msg)
         else:
             print("no message")
-        time.sleep(1)
-        consumer.commit()
+        time.sleep(0.5)
 
 
 if __name__ == "__main__":
@@ -30,12 +38,12 @@ if __name__ == "__main__":
     consumer = Consumer(
         kafka_config
         | {
-            "group.id": "test",
-            "on_commit": lambda err, topics: print(err, topics),
-            "auto.offset.reset": "earliest",
+            "group.id": str(uuid.uuid4()),
+            "on_commit": commit,
+            "enable.auto.commit": False,
+            "enable.partition.eof": True,
         }
     )
-
-    consumer.assign([TopicPartition(TOPIC, 0, 0)])
+    consumer.assign([TopicPartition(TOPIC, 0, -1)])
 
     consume_orders(consumer)
