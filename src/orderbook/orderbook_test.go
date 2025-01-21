b21abe272a376693f82ec84e533fe51e28c3ce68
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2025-01-21 22:51:42 +0800

    add unittests on min heap and max heap

---
+++ src/orderbook/orderbook_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"container/heap"
+	"testing"
+)
+
+func TestMinHeapPop(t *testing.T) {
+	h := &MinHeap{2.0, 1.0, 5.0, 0.0, 6.0, 7.0}
+	wants := []float64{0.0, 1.0, 2.0, 5.0, 6.0, 7.0}
+	heap.Init(h)
+
+	// Test pop
+	count := 0
+	for h.Len() > 0 {
+		got := heap.Pop(h)
+		want := wants[count]
+		if got != want {
+			t.Errorf("got %f, wanted %f for index %d", got, want, count)
+		}
+		count++
+	}
+}
+
+func TestMaxHeapPop(t *testing.T) {
+	h := &MaxHeap{2.0, 1.0, 5.0, 0.0, 6.0, 7.0}
+	wants := []float64{7.0, 6.0, 5.0, 2.0, 1.0, 0.0}
+	heap.Init(h)
+
+	// Test pop
+	count := 0
+	for h.Len() > 0 {
+		got := heap.Pop(h)
+		want := wants[count]
+		if got != want {
+			t.Errorf("got %f, wanted %f for index %d", got, want, count)
+		}
+		count++
+	}
+}
