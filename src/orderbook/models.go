561c9367997702af7d320d7e82570d8196ee54d0
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2025-01-24 22:25:43 +0800

    add models file for various struct of the orderbook

---
+++ src/orderbook/models.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Order struct {
+	OrderID   string  `json:"order_id"`
+	OrderType string  `json:"order_type"`
+	Price     float64 `json:"price"`
+	Quantity  float64 `json:"quantity"`
+	Timestamp float64 `json:"timestamp"`
+}
+
+type Trade struct {
+	OrderId  string  `json:"order_id"`
+	Quantity float64 `json:"quantity"`
+	Price    float64 `json:"price"`
+	Action   string  `json:"action"`
+	Status   string  `json:"status"`
+}
+
+type PricePoint struct {
+	Price float64 `json:"price"`
+}
+
+func (trade Trade) toJSON() []byte {
+	message, err := json.Marshal(trade)
+	if err != nil {
+		fmt.Println("ERROR: invalid trade being converted to message:", err)
+	}
+	return message
+}
+
+func (pricePoint PricePoint) toJSON() []byte {
+	message, err := json.Marshal(pricePoint)
+	if err != nil {
+		fmt.Println("ERROR: invalid price point being converted to message:", err)
+	}
+	return message
+}
+
+func messageToOrder(messageValue []byte) (Order, error) {
+	var order = &Order{}
+	if err := json.Unmarshal(messageValue, order); err != nil {
+		return Order{}, err
+	}
+	return *order, nil
+}
