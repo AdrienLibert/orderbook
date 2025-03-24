1e7797dc35b83bb74aee128e5e79601a1cb81deb
E229866E <adrien.libert@etu.univ-nantes.fr>
2025-03-24 21:36:15 +0800

    added schema

--- src/clickhouse_init/init.sql
+++ src/clickhouse_init/init.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE IF NOT EXISTS analytics;
+CREATE TABLE orderbook_data
+(
+    timestamp DateTime,
+    open Float64,
+    high Float64,
+    low Float64,
+    close Float64,
+    volume Float64
+)
+ENGINE = MergeTree
+ORDER BY timestamp;
